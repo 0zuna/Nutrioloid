f3bcd7e492d60c56e29be98a6b817ec394b2c3e0
Ozuna <oppaozuna@gmail.com>
2016-11-22 12:09:46 -0600

    Update

--- NutrioloID.py
+++ NutrioloID.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #################################################################################
-#  				     Modulo del conocimiento 									#
+#  				     Modulo del conocimiento 			#
 #################################################################################
 #  referencia http://www.mujerdeelite.com/guia_de_alimentos
 class Alimento(object):
@@ -30,7 +30,15 @@ def cargaConocimiento():
 		Alimentos.append(Alimento(alim[0], alim[1], alim[2], alim[3], alim[4], alim[5], 'Verduras'))
 
 #################################################################################
-#  				     Modulo de explicacion										#
+#  				     Modulo de Inferencia			#
+#################################################################################
+def pesoIdeal(sexo, altura):
+    if (sexo=='hombre'):
+        return ((altura - 150)* 0.75)+50
+    elif (sexo=='mujer'):
+        return ((altura - 150)* 0.6)+50
+#################################################################################
+#  				     Modulo de explicacion			#
 #################################################################################
 def valoracion(masa):
 	if (masa<=18):
@@ -39,34 +47,40 @@ Bajo Peso
 
 por lo que debererías seguir un plan encaminado a obtener un peso apto para ti y una correcta nutrición."""
 	elif(masa>18 and masa <=24.9):
-		return """En este momento te encuentras en una situacion de Normo Peso
+            return """En este momento te encuentras en una situacion de Normo Peso
 				Tu peso es adecuado u optimo para tu estatura"""
-	elif (masa>=25 and masa<=26.9):"""
+	elif (masa>=25 and masa<=26.9):
+            return """
 					En este momento te encuentras en una situación de
 			Sobrepeso I
 			
 			Ello quiere decir que tu peso está por encima del recomendado para tu estatura. Accede cuanto antes a inciciar tu plan y verás qué sencillo te resulta"""
-	elif (masa>=27 and masa<=29.9):"""
+	elif (masa>=27 and masa<=29.9):
+            return """
 					En este momento te encuentras en una situación de
 			Sobrepeso II
 			
 			p>Pre-obesidad. Ello quiere decir que tu peso está por encima del recomendado para tu estatura. Accede cuanto antes a inciciar tu plan y verás qué sencillo te resulta."""
-	elif (masa>=30 and masa<= 34.9):"""
+	elif (masa>=30 and masa<= 34.9):
+            return """
 					En este momento te encuentras en una situación de
 			Obesidad Tipo I
 			
 			Ello significa que tu peso está por encima del recomendado para tu estatura, y que además tienes probabilidades de que vaya asociado todo ello a la aparición de patologías (colesterol, Trigliéridos, hipertensión, diabetes tipoII, etc). Por todo esto, accede a iniciar plan, y te ayudaremos a bajar de peso y mejorar tu salud."""
-	elif (masa>=35 and masa<=39.9):"""
+	elif (masa>=35 and masa<=39.9):
+            return """
 					En este momento te encuentras en una situación de
 			Obesidad Tipo II
 			
 			Ello significa que tu peso está por encima del recomendado para tu estatura, y que además tienes probabilidades de que vaya asociado todo ello a la aparición de patologías (colesterol, Trigliéridos, hipertensión, diabetes tipoII, etc). Por todo esto, accede a iniciar plan, y te ayudaremos a bajar de peso y mejorar tu salud."""
-	elif (masa>=40 and masa <= 49.9):"""
+	elif (masa>=40 and masa <= 49.9):
+            return """
 					En este momento te encuentras en una situación de
 			Obesidad Tipo III
 			
 			Ello significa que tu peso está por encima del recomendado para tu estatura, y que además tienes probabilidades de que vaya asociado todo ello a la aparición de patologías (colesterol, Trigliéridos, hipertensión, diabetes tipoII, etc). Por todo esto, accede a iniciar plan, y te ayudaremos a bajar de peso y mejorar tu salud."""
-	elif (masa>=50):"""
+	elif (masa>=50):
+            return """
 					En este momento te encuentras en una situación de
 			Obesidad Extrema
 			
@@ -84,18 +98,19 @@ class Perfil(object):
 		
 def main():
 	print "##############################################################"
-	print "#						NUTRIOLOID							                        #"
+	print "#			    NUTRIOLOID                      #"
 	print "##############################################################"
 	print "Hola antes de empezar porfavor contastame unas preguntas"
 	nombre=raw_input('Cual es tu Nombre>:')
 	peso=raw_input('Cual es tu Peso en kilogramos>:')
 	altura=raw_input('Cual es tu altura en metros>:')
+        sexo=raw_input('Cual es tu sexo>:')
 
 	perfil=Perfil(nombre, float(peso), float(altura))
 	print "%s %s" %(nombre,valoracion(perfil.imc()))
 	cargaConocimiento()
 	Alimentos[0].mostrarAlimento()
-	print len(Alimentos)
+	print pesoIdeal('hombre',1.72)
 	input() 
 
 if __name__ == '__main__':
